dde9d3b265905a6004665b5c100094bec42964a9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2022-02-19 17:18:37 +0100

    my version of prettier settings

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -1,37 +1,37 @@
-import express from "express";
-import bodyParser from "body-parser";
-import nanobuffer from "nanobuffer";
-import morgan from "morgan";
+import express from 'express'
+import bodyParser from 'body-parser'
+import nanobuffer from 'nanobuffer'
+import morgan from 'morgan'
 
 // set up a limited array
-const msg = new nanobuffer(50);
-const getMsgs = () => Array.from(msg).reverse();
+const msg = new nanobuffer(50)
+const getMsgs = () => Array.from(msg).reverse()
 
 // feel free to take out, this just seeds the server with at least one message
 msg.push({
-  user: "brian",
-  text: "hi",
+  user: 'brian',
+  text: 'hi',
   time: Date.now(),
-});
+})
 
 // get express ready to run
-const app = express();
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(express.static("frontend"));
+const app = express()
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(express.static('frontend'))
 
-app.get("/poll", function (req, res) {
+app.get('/poll', function (req, res) {
   res.json({
     msg: getMsgs(),
   })
-});
+})
 
-app.post("/poll", function (req, res) {
-  // add a new message to the server
-  // write code here
-});
+app.post('/poll', function (req, res) {
+  const { user, text } = req.body // This will be from the form submit
+  console.log({ user, text })
+})
 
 // start the server
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log(`listening on http://localhost:${port}`);
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log(`listening on http://localhost:${port}`)
--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -1,18 +1,18 @@
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
+const chat = document.getElementById('chat')
+const msgs = document.getElementById('msgs')
 
 // let's store all current messages here
-let allChat = [];
+let allChat = []
 
 // the interval to poll at in milliseconds
-const INTERVAL = 3000;
+const INTERVAL = 3000
 
 // a submit listener on the form in the HTML
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
-});
+chat.addEventListener('submit', function (e) {
+  e.preventDefault()
+  postNewMsg(chat.elements.user.value, chat.elements.text.value)
+  chat.elements.text.value = ''
+})
 
 async function postNewMsg(user, text) {
   // post to /poll a new message
@@ -28,14 +28,14 @@ function render() {
   // as long as allChat is holding all current messages, this will render them
   // into the ui. yes, it's inefficent. yes, it's fine for this example
   const html = allChat.map(({ user, text, time, id }) =>
-    template(user, text, time, id)
-  );
-  msgs.innerHTML = html.join("\n");
+    template(user, text, time, id),
+  )
+  msgs.innerHTML = html.join('\n')
 }
 
 // given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`
 
 // make the first request
-getNewMsgs();
+getNewMsgs()
